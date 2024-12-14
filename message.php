e14f379b92e8d487071191e76cf234219c71b526
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-12-14 21:18:17 +0800

    add notification creation for messages and ratings; improve SQL query formatting

--- message.php
+++ message.php
@@ -285,6 +285,7 @@ session_start();
     <div class="mainbody">
         <?php
         require 'config.php';
+        require 'utils/utils.php';
 
         $mechname = $_GET['mechname'];
         $username = $_SESSION['fname'] . " " . $_SESSION['lname'];
@@ -295,15 +296,19 @@ session_start();
                 <div class="xxd" style="width:100% ;">
                     <?php
                     if (isset($_POST['send'])) {
+                        $mechId = $_GET['mechid'];
+      
+                        createNotification($mechId, "$username sent a message", 'chat');
                         $mechname = $_GET['mechname'];
                         $username = $_SESSION['fname'] . " " . $_SESSION['lname'];
                         $message = $_POST['message_content'];
-                        $dataset = $connect->query("INSERT INTO `messages`(`msg_sender`, `msg_reciever`, `msg_content`) VALUES ('$username','$mechname','$message')") or die("Error query");
+                        $dataset = $connect->query("INSERT INTO `messages`(`msg_sender`, `msg_receiver`, `msg_content`) VALUES ('$username','$mechname','$message')") or die("Error query");
                     }
                     ?>
 
                     <?php
-                    $sql = ("SELECT * FROM messages WHERE (msg_sender = '$username 'AND msg_reciever = '$mechname') OR (msg_sender = '$mechname' AND msg_reciever = '$username')");
+                    $sql = "SELECT * FROM messages WHERE (msg_sender = '$username' AND msg_receiver = '$mechname') OR (msg_sender = '$mechname' AND msg_receiver = '$username')";
+
                     $dataset = $connect->query($sql);
                     if ($dataset) {
                         if ($dataset->num_rows > 0) {
@@ -320,7 +325,7 @@ session_start();
                         }
                     }
 
-                    $connect->close();
+
 
                     ?>
 
@@ -372,14 +377,18 @@ session_start();
 
                         </div>
                         <?php
-                        require 'config.php';
+                       
                         if (isset($_POST['rate'])) {
+                            $mechId = $_GET['mechid'];
+                            $rating = $_POST['rating'];
+                            $starLabel = $rating > 1 ? 'stars' : 'star';
+                            createNotification($mechId, "$username rated you $rating $starLabel", 'rating');
                             $mech = $_GET['mechid'];
                             $user = $_SESSION['user_id'];
-                            $rating = $_POST['rating'];
-                            $dataset = $connect->query("INSERT INTO `ratings`(`mechanic_id`, `user_id`, `ratings`) VALUES ($mech,$user,$rating)") or die("Error query");
-                            $connect->close();
+                            $dataset = $connect->query("INSERT INTO `ratings`(`mechanic_id`, `user_id`, `ratings`) VALUES ($mechId,$user,$rating)") or die("Error query");
+                           
                         }
+                        $connect->close();
                         ?>
                         </td>
                         <td>
